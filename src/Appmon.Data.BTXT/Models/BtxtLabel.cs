8ca61591310cd0da6c48fc31bdab81dbe410301e
Ere <animeftw@live.co.uk>
2024-03-26 20:13:55 +1100

    Reverse unnecessary change

--- src/Appmon.Data.BTXT/Models/BtxtLabel.cs
+++ src/Appmon.Data.BTXT/Models/BtxtLabel.cs
@@ -2,11 +2,7 @@
 
 public sealed class BtxtLabel
 {
-    public uint EndOffset { get; set; }
-
     public string Key { get; set; } = string.Empty;
-
-    public uint StartOffset { get; set; }
 
     public List<BtxtString> Values { get; init; } = [];
 };
